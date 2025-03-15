caa12d802315446f45bb8292a507770119fd6914
SubSir <tiancaizhangdaxian@sjtu.edu.cn>
2025-03-15 19:55:31 +0800

    feat(os): 移除 buddy_system_allocator 依赖，实现自定义内存分配器
    - 删除了对 buddy_system_allocator crate 的依赖
    - 实现了一个自定义的 Buddy 内存分配器
    - 更新了内存分配的相关配置和代码
    - 优化了内存分配的逻辑，使其更适合操作系统课程的需求

--- os/src/config.rs
+++ os/src/config.rs
@@ -4,7 +4,11 @@ pub const MAX_APP_NUM: usize = 4;
 
 pub const USER_STACK_SIZE: usize = 4096 * 2;
 pub const KERNEL_STACK_SIZE: usize = 4096 * 2;
-pub const KERNEL_HEAP_SIZE: usize = 0x30_0000;
+pub const KERNEL_HEAP_SIZE: usize = 1 << KERNEL_HEAP_WIDTH;
+pub const KERNEL_HEAP_WIDTH: usize = 21;
+pub const NODE_WIDTH: usize = 9;
+pub const BLOCK_SIZE: usize = 1 << (KERNEL_HEAP_WIDTH - NODE_WIDTH);
+pub const NODE_SIZE: usize = 1 << NODE_WIDTH;
 
 pub const CLOCK_FREQ: usize = 12500000;
 pub const TICK_PER_SEC: usize = 100;
--- os/src/mm/heap_allocator.rs
+++ os/src/mm/heap_allocator.rs
@@ -1,16 +1,139 @@
 use crate::config::*;
-use buddy_system_allocator::LockedHeap;
-
-#[global_allocator]
-static HEAP_ALLOCATOR: LockedHeap = LockedHeap::empty();
+use alloc::alloc::{dealloc, GlobalAlloc, Layout};
+use core::ptr::null_mut;
 
 static mut HEAP_SPACE: [u8; KERNEL_HEAP_SIZE] = [0; KERNEL_HEAP_SIZE];
+static mut SIZE_TABEL: [usize; NODE_WIDTH + 1] = [0; NODE_WIDTH + 1];
+static mut NODE_LIST: [Node; 2 * NODE_SIZE] = [Node {
+    next: 0,
+    used: false,
+}; 2 * NODE_SIZE];
+pub struct Buddy;
+#[derive(Copy, Clone)]
+struct Node {
+    next: usize,
+    used: bool,
+}
 
+impl Buddy {
+    pub unsafe fn init(&self) {
+        SIZE_TABEL[0] = 1;
+    }
+
+    fn log_2(&self, mut x: usize) -> usize {
+        assert!(x > 0);
+        let mut i = 0;
+        while x != 0 {
+            x >>= 1;
+            i += 1;
+        }
+        i - 1
+    }
+}
+unsafe impl GlobalAlloc for Buddy {
+    unsafe fn alloc(&self, _layout: Layout) -> *mut u8 {
+        // ignore align!
+        let mut size = _layout.size();
+        // println!("alloc: {}", size);
+        if size < BLOCK_SIZE {
+            size = 1;
+        } else {
+            size /= BLOCK_SIZE;
+        }
+        let mut align = _layout.align();
+        // println!("align: {}", align);
+        if align < BLOCK_SIZE {
+            align = 1;
+        } else {
+            align /= BLOCK_SIZE;
+        }
+        let width = NODE_WIDTH - self.log_2(size);
+        let mut i = width as isize;
+        // println!("width: {}, size: {}, align: {}", i, size, align);
+        while i >= 0 && SIZE_TABEL[i as usize] == 0 {
+            i -= 1;
+        }
+        if i < 0 {
+            return null_mut();
+        }
+        println!("i: {}", i);
+        let mut father = SIZE_TABEL[i as usize];
+        SIZE_TABEL[i as usize] = NODE_LIST[father].next;
+        NODE_LIST[father].used = true;
+        for j in (i + 1) as usize..=width {
+            SIZE_TABEL[j] = father * 2 + 1;
+            father = father * 2;
+            NODE_LIST[father].used = true;
+        }
+        // println!("father: {}", father);
+        let addr = (father - (1 << width)) * BLOCK_SIZE * size;
+        let base_ptr = HEAP_SPACE.as_mut_ptr();
+        let target_ptr = base_ptr.add(addr);
+        // println!(
+        //     "alloc: {}, base_ptr: {:x}, target_ptr: {:x}",
+        //     addr, base_ptr as usize, target_ptr as usize
+        // );
+        target_ptr
+    }
+
+    unsafe fn dealloc(&self, _ptr: *mut u8, _layout: Layout) {
+        let mut size = _layout.size();
+        if size < BLOCK_SIZE {
+            size = 1;
+        } else {
+            size /= BLOCK_SIZE;
+        }
+        let width = NODE_WIDTH - self.log_2(size);
+        let base_ptr = HEAP_SPACE.as_mut_ptr();
+        let addr = (_ptr as usize - base_ptr as usize) / 8 / BLOCK_SIZE;
+        let node = addr + (1 << width);
+        let buddy = node ^ 1;
+        // println!(
+        //     "dealloc: {:x}, node: {}, buddy: {}",
+        //     _ptr as usize, node, buddy
+        // );
+        NODE_LIST[node].used = false;
+        if buddy == 0 {
+            SIZE_TABEL[0] = 1;
+            return;
+        }
+        if NODE_LIST[buddy].used == false {
+            let mut head = SIZE_TABEL[width];
+            if head == buddy {
+                SIZE_TABEL[width] = NODE_LIST[buddy].next;
+            } else {
+                while NODE_LIST[head].next != 0 && NODE_LIST[head].next != buddy {
+                    head = NODE_LIST[head].next;
+                }
+                NODE_LIST[head].next = NODE_LIST[buddy].next;
+            }
+            let mut _layout_ =
+                Layout::from_size_align_unchecked(size * BLOCK_SIZE * 2, _layout.align());
+            self.dealloc(_ptr, _layout_);
+        } else {
+            if SIZE_TABEL[width] == 0 {
+                SIZE_TABEL[width] = node;
+                NODE_LIST[node].next = 0;
+                return;
+            }
+            let mut head = SIZE_TABEL[width];
+            if head == 0 {
+                SIZE_TABEL[width] = node;
+            } else {
+                while NODE_LIST[head].next != 0 && NODE_LIST[head].next < node {
+                    head = NODE_LIST[head].next;
+                }
+                NODE_LIST[node].next = NODE_LIST[head].next;
+                NODE_LIST[head].next = node;
+            }
+        }
+    }
+}
+#[global_allocator]
+static HEAP_ALLOCATOR: Buddy = Buddy {};
 pub fn init_heap() {
     unsafe {
-        HEAP_ALLOCATOR
-            .lock()
-            .init(HEAP_SPACE.as_ptr() as usize, KERNEL_HEAP_SIZE);
+        HEAP_ALLOCATOR.init();
     }
 }
 
